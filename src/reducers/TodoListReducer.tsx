ff960142e4ce7ef11dbedc268a0c4ef9349c980a
Patrick Dinh <d.conghoang@gmail.com>
2017-07-15 11:19:21 +1000

    Make things a bit nicer

--- src/reducers/TodoListReducer.tsx
+++ src/reducers/TodoListReducer.tsx
@@ -3,14 +3,13 @@ import { StoreState } from '../types/index';
 import { ADD_TODO } from '../constants/index';
 import { TodoObj } from "../components/Todo";
 
-export function addToDoReducer(state: StoreState, action: AddTodoAction): StoreState {
+export function todoListReducer(state: StoreState, action: AddTodoAction): StoreState {
   switch (action.type) {
     case ADD_TODO:
-      let newTodoList = state.todoList.slice(0);
       const newId = state.todoList.length + 1;
-      newTodoList.push(new TodoObj(newId, `Learn another thing id = ${newId}`));
+      const newTodo = new TodoObj(newId, `Learn another thing id = ${newId}`);
 
-      return Object.assign({}, state, { todoList: newTodoList });
+      return Object.assign({}, state, { todoList: [...state.todoList,  newTodo] });
   }
 
   return state;
