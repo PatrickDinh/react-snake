aa7997529d7c14955a750f485fc39e7d91f6c1ed
Patrick Dinh <d.conghoang@gmail.com>
2017-08-06 20:21:49 +1000

    Small refactoring in the reducer

--- src/reducers/TodoListReducer.tsx
+++ src/reducers/TodoListReducer.tsx
@@ -3,15 +3,25 @@ import { StoreState } from '../types/index';
 import * as constants from '../constants/index';
 import TodoModel from "../models/TodoModel";
 
+function addNewTodo(state: StoreState, name: string): StoreState {
+  // TODO: come up with a better way to create new state as object.assign returns object
+  const newTodo = new TodoModel(state.todoList.length + 1, name);
+  return Object.assign({}, state, { todoList: [...state.todoList, newTodo] });
+}
+
+function updateTodoStatus(state: StoreState, id: number, completed: boolean): StoreState {
+  let todo = state.todoList.find(t => t.id === id);
+  if (todo) todo.completed = completed;
+  // TODO: come up with a better way to create new state as object.assign returns object
+  return Object.assign({}, state, { todoList: [...state.todoList] });
+}
+
 export function todoListReducer(state: StoreState, action: TodoActions): StoreState {
   switch (action.type) {
     case constants.ADD_TODO:
-      const newTodo = new TodoModel(state.todoList.length + 1, action.value);
-      return Object.assign({}, state, { todoList: [...state.todoList,  newTodo] });
+      return addNewTodo(state, action.value);
     case constants.UPDATE_TODO_STATUS:
-      let todo = state.todoList.find(t => t.id === action.id);
-      if (todo) todo.completed = action.completed;
-      return Object.assign({}, state, { todoList: [...state.todoList] });
+      return updateTodoStatus(state, action.id, action.completed);
   }
 
   return state;
