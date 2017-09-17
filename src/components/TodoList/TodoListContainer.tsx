7dd36f33d12c21c2241a16ab98595ca44095fa7a
Patrick Dinh <d.conghoang@gmail.com>
2017-09-17 22:34:39 +1000

    Move timeOut to a common service

--- src/components/TodoList/TodoListContainer.tsx
+++ src/components/TodoList/TodoListContainer.tsx
@@ -5,16 +5,13 @@ import { StoreState } from '../../types/index';
 import { Dispatch } from 'redux';
 import TodoModel from '../../models/TodoModel';
 import { ThunkAction } from 'redux-thunk';
+import { TimeOut } from '../../common/time-out';
 
 export function mapStateToProps(state: StoreState) {
   return { 
     todoList: state.todoList
   };
 }
-
-const timeOut = (ms: number): Promise<{}> => {
-  return new Promise(resolve => setTimeout(resolve, ms));
-};
 
 const getNewTodoModel = (name: string): ThunkAction<Promise<TodoModel>, StoreState, null> => {
   return (dispatch: Dispatch<StoreState>, getState: () => StoreState) => {
@@ -25,15 +22,15 @@ const getNewTodoModel = (name: string): ThunkAction<Promise<TodoModel>, StoreSta
 const addTodoWithAnimation = async (dispatch: Dispatch<StoreState>, name: string) => {
   let model = await dispatch(getNewTodoModel(name));
   await dispatch(actions.addTodo(model));
-  await timeOut(10);
+  await TimeOut(10);
   dispatch(actions.updateToDoVisibility(model.id, true));
 };
 
 const updateTodoStatusWithAnimation = async (dispatch: Dispatch<StoreState>, id: number, completed: boolean) => {
   dispatch(actions.updateToDoVisibility(id, false));
-  await timeOut(300);
+  await TimeOut(300);
   dispatch(actions.updateTodoStatus(id, completed));
-  await timeOut(30);
+  await TimeOut(30);
   dispatch(actions.updateToDoVisibility(id, true));
 };
 
